45d6ca2c6984070cae8c28d570888a1a3538480b
ayou <ayou@ayoudeMac-mini.local>
2016-09-19 21:44:28 +0800

    增加birthday页面

--- themes/yaht/source/js/birthday.js
+++ themes/yaht/source/js/birthday.js
@@ -4,13 +4,19 @@
  ;(function(){
    $(function(){
      $("#myWords").lbyl({
-          content: "亲爱的小妹纸，第一个生日，男朋友就不在身边，依我看，这个男朋友还不如休了算了。"+
-            "其实，男朋友真的好想能给你过一个生日。"+
-            "身在异国他乡，希望你能开开心心（男朋友尽量不惹你生气）地过每一天，好好照顾好自己。",
+          content: "亲爱的小妹纸，生日快乐！只能送一个假蛋糕给你了，第一个生日，男朋友就不在身边，依我看，这个男朋友还不如休了算了。^-^"+
+            "外面的世界很精彩，希望你能开开心心地去领略，好好照顾好自己。",
           speed: 300, //time between each new letter being added
           type: 'show', // 'show' or 'fade'
           fadeSpeed: 500, // Only relevant when the 'type' is set to 'fade'
-          finished: function(){ console.log('finished') } // Finished Callback
+          finished: function(){
+            $("#sign").lbyl({
+                 content: "I Love you",
+                 speed: 300, //time between each new letter being added
+                 type: 'show', // 'show' or 'fade'
+                 fadeSpeed: 500, // Only relevant when the 'type' is set to 'fade'
+               });
+          } // Finished Callback
       });
    });
- })()
+ })();
